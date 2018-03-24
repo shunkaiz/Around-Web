a45c1cda954e576e4ede9f90e3cf7b1c6df1d6f2
shunkaiz <shunkaiz@usc.edu>
2018-03-23 23:44:27 -0700

    register implementation

---
+++ src/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Register} from './Register'
+export class Main extends React.Component{
+    render(){
+        return(
+          <Register/>
+        );
+    }
+}
