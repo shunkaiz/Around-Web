98b16941c4ecce97d453efdcda86606e8c55482f
shunkaiz <shunkaiz@usc.edu>
2018-03-26 18:51:32 -0700

    url fix

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,19 +2,27 @@ import React from 'react';
 import {Register} from './Register';
 import {Login} from './Login';
 import {Home} from './Home';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
 export class Main extends React.Component{
     getLogin = () =>{
-        return this.props.isLoggedIn?<Home/>:<Login handleLogin = {this.props.handleLogin}/>;
+        return this.props.isLoggedIn?<Redirect to='/home/'/>:<Login handleLogin = {this.props.handleLogin}/>;
     }
 
+    getHome = () =>{
+        return this.props.isLoggedIn?<Home/>: <Redirect to ='/login'/>;
+    }
+
+    getRoot =() =>{
+        return <Redirect to ='/login'/>
+    }
     render(){
         return(
             <Switch>
-                <Route exact path="/" render={this.getLogin}/>
+                <Route exact path="/" render={this.getRoot}/>
                 <Route path="/login" render={this.getLogin}/>
                 <Route path="/register" component={Register}/>
-                <Route component = {Login}/>
+                <Route path ='/home' render ={this.getHome}/>
+                <Route render={this.getRoot}/>
             </Switch>
         );
     }
