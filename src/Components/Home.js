1d2816d0007dfb5afeb8a119f33b2a3776a7d006
shunkaiz <shunkaiz@usc.edu>
2018-04-01 12:29:02 -0700

    add geo function in hone js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { Tabs, Button} from 'antd';
+import {GEO_OPTIONS} from "../constants";
 
 export class Home extends React.Component {
+
+    getGeoLocation = () =>{
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            this.setState({ error: 'Your browser does not support geolocation!' });
+        }
+
+    }
+
+    onSuccessLoadGeoLocation = (position) =>{
+        console.log(position);
+    }
+
+    onFailedLoadGeoLocation = () =>{
+
+    }
+
+    componentDidMount(){
+        this.getGeoLocation();
+    }
+
     render() {
         const operations = <Button type={'primary'}>Create My New Post</Button>;
         const TabPane = Tabs.TabPane;
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const API_ROOT = 'http://around-195521.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge        : 30000,
+    timeout           : 27000
+};
